0ba5a9119d05abf711149a94d644ee4974937e73
Chappellm <Chappelm1@hawkmail.newpaltz.edu>
2022-01-29 17:37:23 -0500

    updated to async functions

--- server/models/images.js
+++ server/models/images.js
@@ -6,28 +6,29 @@ module.exports.collection = collection;
 const addOwnerPipeline = [
     {
         $lookup: {
-            from: 'users',
+            from: 'SagieImages',
             localField: 'owner',
             foreignField: '_id',
             as: 'owner'
         }
     },
-    { $unwind: '$owner' }
+    { $unwind: '$owner' },
+    { $project: { "owner.password": 0 } }
 ];
 
 // get all images
-module.exports.getAll = () => {
+module.exports.getAll = async function getAll() {
     return collection.aggregate(addOwnerPipeline).toArray();
 }
 
 // get by id
-module.exports.getById = (id) => {
+module.exports.getById = async function getById(id) {
     return collection.aggregate(addOwnerPipeline).toArray().then((images) => {
         return images.find(image => image._id == id);
     });
 }
 
 // post a new image to the database
-module.exports.create = (image) => {
+module.exports.create = async function create(image) {
     return collection.insertOne(image);
 }
